8229e862133d8fa5247107ab6e7e021b6e459c2c
Селезнева Людмила Владимировна <LVlSelezneva@sberbank.ru>
2020-06-29 09:24:50 +0300

    L23_Spring_ORM

--- L21_SpringBoot_adv/src/main/java/ru/selezneva/L21_SpringBoot_adv/atm/exceptions/SavingATMToFileException.java
+++ L21_SpringBoot_adv/src/main/java/ru/selezneva/L21_SpringBoot_adv/atm/exceptions/SavingATMToFileException.java
@@ -1,6 +1,6 @@
 package ru.selezneva.L21_SpringBoot_adv.atm.exceptions;
 
-public class SavingATMToFileException extends RuntimeException {
+public class SavingATMToFileException extends ATMException {
     public SavingATMToFileException(Throwable cause) {
         super(cause);
     }
